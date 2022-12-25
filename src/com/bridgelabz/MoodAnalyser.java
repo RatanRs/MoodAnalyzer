ff647556dbbcfb45d76e09c64d405d7a2ece84f7
9663461085 <ratansollapure16310@gmail.com>
2022-12-25 19:53:38 +0530

    Added:Used constractor to print mood happy or sad

--- src/com/bridgelabz/MoodAnalyser.java
+++ src/com/bridgelabz/MoodAnalyser.java
@@ -12,24 +12,28 @@ public class MoodAnalyser {
 	 * 
 	 * @param args
 	 */
-	public String moodAnalyzer(String message) {
-        /**
-         * In this method return mood
-         */
-        boolean status = message.contains("sad");
-        if (status == true) {
-            return "Sad";
-        } else {
-            return "Happy";
-        }
-    }
+	String message;
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter message : ");
-        String message = input.nextLine();
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String result = moodAnalyser.moodAnalyzer(message);
-        System.out.println(result);
-    }
+	public MoodAnalyser(String message) {
+		this.message = message;
+	}
+
+	public String moodAnalyzer() {
+
+		boolean status = message.toLowerCase().contains("sad");
+		if (status == true) {
+			return "Sad";
+		} else {
+			return "Happy";
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter message : ");
+		String message = input.nextLine();
+       MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+		String result = moodAnalyser.moodAnalyzer();
+		System.out.println(result);
+	}
 }
