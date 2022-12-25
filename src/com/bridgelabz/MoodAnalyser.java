796ae2f347e35987a8d09a4edbea709793a13f93
9663461085 <ratansollapure16310@gmail.com>
2022-12-25 19:48:18 +0530

    Added:Welcome meassage to Modanalyzer programm

---
+++ src/com/bridgelabz/MoodAnalyser.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+/***
+ * 
+ * @author Admin
+ *
+ */
+public class MoodAnalyser {
+	/***
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to MoodAnalyser Programm");
+	}
+}
