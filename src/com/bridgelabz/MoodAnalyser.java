465119016a8e4526a1280251ec857434cf22fff4
9663461085 <ratansollapure16310@gmail.com>
2022-12-25 19:50:34 +0530

    Added:Checking or mood happy or sad

--- src/com/bridgelabz/MoodAnalyser.java
+++ src/com/bridgelabz/MoodAnalyser.java
@@ -1,4 +1,7 @@
 package com.bridgelabz;
+
+import java.util.Scanner;
+
 /***
  * 
  * @author Admin
@@ -9,7 +12,24 @@ public class MoodAnalyser {
 	 * 
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		System.out.println("Welcome to MoodAnalyser Programm");
-	}
+	public String moodAnalyzer(String message) {
+        /**
+         * In this method return mood
+         */
+        boolean status = message.contains("sad");
+        if (status == true) {
+            return "Sad";
+        } else {
+            return "Happy";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter message : ");
+        String message = input.nextLine();
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String result = moodAnalyser.moodAnalyzer(message);
+        System.out.println(result);
+    }
 }
